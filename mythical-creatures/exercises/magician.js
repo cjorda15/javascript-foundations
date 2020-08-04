b9f599b52b5ab221ce1fb3e2001fe9d6b8c1a433
Chris Jordan <crobertjordan@yahoo.com>
2020-08-04 09:42:32 -0600

    take that magician

---
+++ mythical-creatures/exercises/magician.js
@@ -0,0 +1,23 @@
+module.exports =  class Magician{
+  constructor({name,topHat}={}){
+    this.name = name || "Chris"
+    this.topHat = topHat!==undefined?topHat: true
+    this.confident = false
+    this.spellCount =0
+
+  }
+
+  incantation(spell){
+    return `${spell.toUpperCase()}!`
+  }
+
+  cast(){
+    this.spellCount++
+    this.confident = this.spellCount>=3
+    return this.topHat? 'PULL RABBIT FROM TOP HAT':'PULL DOVE FROM SLEEVE'
+  }
+
+  performShowStopper(){
+    return this.confident?'WOW! The magician totally just sawed that person in half!' :'Oh no! Practice more before attempting this trick!'
+  }
+}
